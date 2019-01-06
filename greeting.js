1980a601fc761d0060fb8efcd4aea226b1dc498a
Mark42 <dc7303@gmail.com>
2019-01-06 16:39:27 +0900

    toDoList 동적 생성 구현 완료

--- greeting.js
+++ greeting.js
@@ -5,27 +5,33 @@ const greeting = document.querySelector('.js-greetings');
 const USER_LS = 'currentUser';
 const SHOWING_CN = 'showing';
 
+//Name 저장 func
 function saveName(text) {
   localStorage.setItem(USER_LS, text);
 }
 
+//input submit event prevent & event set
 function handleSubmit(event) {
   event.preventDefault();
   const currentValue = input.value;
   paintGreeting(currentValue);
   saveName(currentValue);
 }
+
+//localStorage(currentUser) Null일때 form showing
 function askForName() {
   form.classList.add(SHOWING_CN);
   form.addEventListener('submit', handleSubmit);
 }
 
+//localStorage(currentUser) Null이 아닐 때 greeting showing
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
   greeting.innerText = `Hello ${text}`;
 }
 
+//Load func
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
